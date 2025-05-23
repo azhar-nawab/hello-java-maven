c4b0c47b6b0fede8542ef43460bcdc2bd7e75dca
azhar-nawab <azharnawab79@gmail.com>
2025-05-23 13:14:10 +0530

    Create HelloTest.java

---
+++ src/test/java/com/example/HelloTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
